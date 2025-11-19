0050d7b4a0eb0f7893cc02239a72649fd5a797ac
Xx-Diego-XX-Agustin-xx-Garcia-xX <116301540+Xx-Diego-XX-Agustin-xx-Garcia-xX@users.noreply.github.com>
2025-11-19 13:31:18 -0800

    Create Employee class
    Created a class to generate an employee, complete with first and last name.

---
+++ Employee.java
@@ -0,0 +1,11 @@
+public class Employee
+{
+    private String firstName;
+    private String lastName;
+
+    public Employee(String theirFirstName, String theirLastName)
+    {
+        firstName = theirFirstName;
+        lastName = theirLastName;
+    }
+}
